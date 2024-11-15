899a39bd8d88ce32e17b7545f3355a3d274731db
Smirnov Danila <danilasmirnov2077@gmail.com>
2024-11-15 05:22:47 +0500

    auth 1.2.1 ghp rework

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "/CRM-System/",
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: [
       {
